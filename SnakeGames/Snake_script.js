0c7d4ed9285ed1fe5a083c5e322de8d559bccc87
beluga-T <tswlovexyb@outlook.com>
2024-04-23 23:32:50 -0700

    Refactor Snake game controls for better user experience

--- SnakeGames/Snake_script.js
+++ SnakeGames/Snake_script.js
@@ -30,16 +30,16 @@ const handleGameOver = () => {
 
 const changeDirection = e => {
     // Changing velocity value based on key press
-    if (e.key === "ArrowUp" && velocityY != 1) {
+    if ((e.key === "ArrowUp" || e.key === "w") && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if (e.key === "ArrowDown" && velocityY != -1) {
+    } else if ((e.key === "ArrowDown" || e.key === "s") && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if (e.key === "ArrowLeft" && velocityX != 1) {
+    } else if ((e.key === "ArrowLeft" || e.key === "a") && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if (e.key === "ArrowRight" && velocityX != -1) {
+    } else if ((e.key === "ArrowRight" || e.key === "d") && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
